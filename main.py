9f57aa045b78d4b70257689b162c11764c3f0536
slaurence1003 <sabine.a.laurence@gmail.com>
2025-11-19 15:02:49 -0500

    pseudo code

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
-
+import pandas as pd
+import random
     
-def train():
-    #choose 5-8 random ingredients
+def train(df):
+    all_ingredients = df['name'].tolist()
+
+    num_to_pick = random.randint(5, 8)
+    ingredients = random.sample(all_ingredients, num_to_pick)
+    print(ingredients)
     #get some reward from the combination (based on price, nutritional value, etc)
+    #save reward to Q-table for given combo of ingredients (state)
     #swap an ingredient out and get a new reward- save to table
-    #continuing swapping until some end state/optimal value is found
+    #continuing swapping NEED TO DETERMINE "DONE" STATE
+    #use Q-learning algorithm with decay rate, etc.
     
     
 
 if __name__ == "__main__":
-    train()
+    # based on user input choose between tj/wf
+    df = pd.read_csv('trader_joes_nov11.csv')
+    train(df)
